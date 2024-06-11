6d0a8d7182c1e05a9ff1a3acc7b079717b513168
phitu2211 <phitu2211@gmail.com>
2024-06-11 14:59:50 +0700

    add db

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -1,20 +1,40 @@
 package com.example.demo.controllers;
 
-import com.example.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.ArrayList;
+import com.example.demo.models.Student;
+import com.example.demo.repositories.StudentRepo;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class DemoController {
-  @GetMapping("/hello")
-  public ResponseEntity<List<Student>> hello() {
-    List<Student> students = new ArrayList<>();
-    students.add(new Student(1L, "Anh", "phitu2211@gmail.com", 18));
-    students.add(new Student(2L, "Phi", "phitu2211@gmail.com", 20));
-    return ResponseEntity.ok(students);
+  @Autowired
+  private StudentRepo repo;
+
+  @GetMapping("/students")
+  public ResponseEntity<List<Student>> listStudents() {
+    return ResponseEntity.ok(repo.findAll());
+  }
+
+  public int randInt(int min, int max) {
+    Random rand = new Random();
+    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+    return randomNum;
+  }
+
+  @GetMapping("/student/create")
+  public ResponseEntity<Student> create() {
+    Student student = new Student();
+    student.setAge(randInt(10, 100));
+    student.setName("Anh " + randInt(100, 100000));
+    student.setEmail(randInt(10, 500000) + "@gmail.com");
+    repo.save(student);
+    return ResponseEntity.ok(student);
   }
 
 }
--- src/main/java/com/example/demo/models/Student.java
+++ src/main/java/com/example/demo/models/Student.java
@@ -1,6 +1,14 @@
-package com.example.models;
+package com.example.demo.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "student")
 public class Student {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   public Long getId() {
